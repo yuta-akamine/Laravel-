54e25f65fed1b14806a82565a0187af476fbf334
yuta-akamine <happykaizokuou@gmail.com>
2023-04-18 21:09:29 +0900

    ヒアドキュメントにて出力テスト

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+$html = <<<EOF
+<html>
+    <head>
+        <title>hello</title>
+        <style>
+            body {font-size: 16pt; color: #999;}
+            h1 {font-size: 100px; text-align: right; color: #eee; margin: -40px 0px -50px 0px;}
+        </style>
+    </head>
+    <body>
+        <h1>hello</h1>
+        <p>This is sample page.</p>
+        <p>これは、サンプルで作ったページです。</p>
+    </body>
+</html>
+EOF;
+
+Route::get('hello', function () use ($html) {
+    return $html;
+});
