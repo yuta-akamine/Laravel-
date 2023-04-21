377fc6859acfc4c420d01da61397870d8197ec15
yuta-akamine <happykaizokuou@gmail.com>
2023-04-21 21:27:54 +0900

    任意パラメータテスト

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-$html = <<<EOF
-<html>
-    <head>
-        <title>hello</title>
-        <style>
-            body {font-size: 16pt; color: #999;}
-            h1 {font-size: 100px; text-align: right; color: #eee; margin: -40px 0px -50px 0px;}
-        </style>
-    </head>
-    <body>
-        <h1>hello</h1>
-        <p>This is sample page.</p>
-        <p>これは、サンプルで作ったページです。</p>
-    </body>
-</html>
-EOF;
 
-Route::get('hello', function () use ($html) {
+Route::get('hello/{msg?}', function ($msg='no message.') {
+
+    $html = <<<EOF
+    <html>
+        <head>
+            <title>hello</title>
+            <style>
+                body {font-size: 16pt; color: #999;}
+                h1 {font-size: 100px; text-align: right; color: #eee; margin: -40px 0px -50px 0px;}
+            </style>
+        </head>
+        <body>
+            <h1>hello</h1>
+            <p>{$msg}</p>
+            <p>これは、サンプルで作ったページです。</p>
+        </body>
+    </html>
+    EOF;
+
     return $html;
 });
